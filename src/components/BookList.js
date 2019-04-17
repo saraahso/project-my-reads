e53fb0f10700eb5ec6381a009cbfc4bfdfa5946d
Sara S. de Oliveira <saraahso@gmail.com>
2019-04-17 16:02:36 +0000

    Separate code into components

---
+++ src/components/BookList.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+
+class BookList extends Component {
+  render (){
+    return (
+      <div className="list-books">
+            <div className="list-books-title">
+              <h1>MyReads</h1>
+            </div>
+            <div className="list-books-content">
+              <div>
+                
+                
+                
+              </div>
+            </div>
+            <div className="open-search">
+              <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
+            </div>
+          </div>
+    )
+  }
+}
+
+export default BookList
